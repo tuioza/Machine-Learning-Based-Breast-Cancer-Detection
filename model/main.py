c8a08cc366d69ff88967d6fdcd431c54ad4545c0
tuioza <141247530+tuioza@users.noreply.github.com>
2023-12-31 22:41:26 +0100

    Update main.py

--- model/main.py
+++ model/main.py
@@ -1,13 +1,26 @@
-import pandas as pd 
+import pandas as pd
 
 
-def clean_data():
-    data = pd.read_csv("data\data.csv")
-    print(data.head())
-    return data
+def create_model():
+    return
+
+# Fonction pour nettoyer les données
+def clean_data() -> pd.DataFrame:
+    # Lecture du fichier CSV
+    data: pd.DataFrame = pd.read_csv("data/data.csv")
+    
+    # Suppression des colonnes 'Unnamed: 32' et 'id'
+    data = data.drop(['Unnamed: 32', 'id'], axis=1)
+    
+    # Mapping des valeurs de la colonne 'diagnosis' de 'M' à 1 et 'B' à 0
+    data['diagnosis'] = data['diagnosis'].map({'M': 1, 'B': 0})
+    
+    return data  # Retourne les données nettoyées au format DataFrame
+
 
 def main():
     data = clean_data()
+ 
     return
 
 if __name__ == '__main__':
